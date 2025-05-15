707abb3282334da60249db2a8ea0665d10ece21e
Ithran Beor Turno <turno.ithranbeor7@gmail.com>
2025-05-15 19:41:11 +0800

    Refactor password reset templates and update URLs for improved structure and consistency

--- LearnIT/users/urls.py
+++ LearnIT/users/urls.py
@@ -56,9 +56,9 @@ urlpatterns = [
     path('search-signin-history/', views.search_signin_history, name='search_signin_history'),
     path('delete-signin/<int:activity_id>/', views.delete_signin_activity, name='delete_signin_activity'),
     path('terms/', views.terms, name='terms'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
